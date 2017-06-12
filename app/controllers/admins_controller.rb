4fd6883070561f8c6211ca01f090523ef0c677ee
Masaya Yoshino <masayaliketennis@yahoo.co.jp>
2017-06-12 05:53:11 +0000

    画像登録

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -7,13 +7,4 @@ class AdminsController < ApplicationController
     @admin = Admin.find(params[:id])
   end
   
-  def update
-    @admin = Admin.find(params[:id])
-    @admin.assign_attributes(params[:admin])
-    if @admin.save
-            redirect_to @admin, notice:"会員情報を更新しました。"
-        else
-            render "edit"
-        end
-    end
 end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,4 +4,13 @@ class Admin < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   
+  def set_image(file)
+    if !file.nil?
+      file_name = file.original_filename
+      File.open("public/admin_images/#{file_name}", 'wb') { |f|
+        f.write(file.read)
+      }
+      self.image = file_name
+    end
+  end
 end
