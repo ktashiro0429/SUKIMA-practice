419ed92a20dc05fbf14d736b66e9142a9b309563
TATSUYA <tatsuya415077@gmail.com>
2017-06-16 22:26:26 +0900

    グループcreate時の重複防止機能、アカウント登録時のvalidation追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20170614153331) do
 
-ActiveRecord::Schema.define(version: 20170612124340) do
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
