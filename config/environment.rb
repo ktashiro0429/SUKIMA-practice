0cb00941b430c6f67490472a0294ee72ff9d3ca4
Masaya Yoshino <masayaliketennis@yahoo.co.jp>
2017-09-15 15:32:46 +0000

    adminsのローカルでの登録を可能に

--- config/environment.rb
+++ config/environment.rb
@@ -5,12 +5,12 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => 'app76421410@heroku.com',
-  :password       => 'tqclydz18009',
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
-}
+#ActionMailer::Base.smtp_settings = {
+#  :address        => 'smtp.sendgrid.net',
+#  :port           => '587',
+##  :authentication => :plain,
+#  :user_name      => 'app76421410@heroku.com',
+#  :password       => 'tqclydz18009',
+#  :domain         => 'heroku.com',
+#  :enable_starttls_auto => true
+#}
