925014b4c4fbbc85190fcf318728f99674bdca50
Masaya Yoshino <masayaliketennis@yahoo.co.jp>
2017-09-15 19:15:18 +0000

    メール設定を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,8 +79,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :user_name => "tatsuya415077@gmail.com",
-    :password => "tatsuya4150",
+    :user_name => ENV['MAIL_USER_NAME'],
+    :password => ENV['MAIL_PASSWORD'],
     :authentication => :plain,
     :enable_starttls_auto => true
   
